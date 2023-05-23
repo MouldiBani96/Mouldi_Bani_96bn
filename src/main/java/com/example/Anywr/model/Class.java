7bc12576e06196e323848231a9b82c40a8fb018d
MouldiBani96 <bani.mouldi96@gmail.com>
2023-05-23 15:17:21 +0200

    add lombok in entity for geter and setter

--- src/main/java/com/example/Anywr/model/Class.java
+++ src/main/java/com/example/Anywr/model/Class.java
@@ -1,11 +1,14 @@
 package com.example.Anywr.model;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Data
 public class Class {
 
     @Id
