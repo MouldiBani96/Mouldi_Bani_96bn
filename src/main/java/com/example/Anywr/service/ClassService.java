2747410263f1f01aa906e7d14e257f632e387fe7
MouldiBani96 <bani.mouldi96@gmail.com>
2023-05-23 15:16:10 +0200

    create services

--- src/main/java/com/example/Anywr/service/ClassService.java
+++ src/main/java/com/example/Anywr/service/ClassService.java
@@ -1,4 +1,36 @@
 package com.example.Anywr.service;
 
+import com.example.Anywr.model.Class;
+import com.example.Anywr.repository.ClassRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ClassService {
+
+    @Autowired
+    private ClassRepository classRepository;
+
+    public List<Class> getAllClasses() {
+        return classRepository.findAll();
+    }
+
+    public Class getClassById(Long id) {
+        return classRepository.findById(id).orElse(null);
+    }
+
+    public Class createClass(Class cls) {
+        return classRepository.save(cls);
+    }
+
+    public Class updateClass(Long id, Class cls) {
+        cls.setId(id);
+        return classRepository.save(cls);
+    }
+
+    public void deleteClass(Long id) {
+        classRepository.deleteById(id);
+    }
 }
--- src/main/java/com/example/Anywr/service/TeacherService.java
+++ src/main/java/com/example/Anywr/service/TeacherService.java
@@ -1,7 +1,36 @@
 package com.example.Anywr.service;
 
+import com.example.Anywr.model.Teacher;
+import com.example.Anywr.repository.TeacherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TeacherService {
 
+    @Autowired
+    private TeacherRepository teacherRepository;
 
+    public List<Teacher> getAllTeachers() {
+        return teacherRepository.findAll();
+    }
 
+    public Teacher getTeacherById(Long id) {
+        return teacherRepository.findById(id).orElse(null);
+    }
+
+    public Teacher createTeacher(Teacher teacher) {
+        return teacherRepository.save(teacher);
+    }
+
+    public Teacher updateTeacher(Long id, Teacher teacher) {
+        teacher.setId(id);
+        return teacherRepository.save(teacher);
+    }
+
+    public void deleteTeacher(Long id) {
+        teacherRepository.deleteById(id);
+    }
 }
