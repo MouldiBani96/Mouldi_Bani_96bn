e57d3d981b61b98f3b0795f53c7a0713f2b5e7a0
MouldiBani96 <bani.mouldi96@gmail.com>
2023-05-23 15:08:52 +0200

    create entity

--- src/main/java/com/example/Anywr/model/Class.java
+++ src/main/java/com/example/Anywr/model/Class.java
@@ -1,4 +1,19 @@
 package com.example.Anywr.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Class {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    // Constructors, getters, and setters
+
+    // ...
 }
--- src/main/java/com/example/Anywr/model/Student.java
+++ src/main/java/com/example/Anywr/model/Student.java
@@ -1,4 +1,20 @@
 package com.example.Anywr.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    // Constructors, getters, and setters
+
+    // ...
 }
